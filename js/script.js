30368690e78fac34b52cf14b94fe65ea67929aac
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-11 14:05:30 +0200

    created an empty array to push the random emails into and created a "v-for" in html to print each email in a different <li>

--- js/script.js
+++ js/script.js
@@ -9,3 +9,31 @@ Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
  */
 
+const app = new Vue(
+    {
+        el: "#app",
+
+        data: {
+            
+            emailArr: []
+
+        },
+
+        mounted(){
+          this.randomEmails()
+        },
+
+        methods: {
+            // FUNZIONE CHE CHIAMA L'API PER GENERARE UNA MAIL CASUALE E PUSHARE NELL'ARRAY DEI DATA(GENERA 10 EMAIL CASUALI CON QUESTO CICLO)
+            randomEmails(){
+                for(let i = 0; i < 10; i++){
+                    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then(res => {
+                        this.emailArr.push(res.data.response)
+                    })
+                }
+                console.log(this.emailArr)
+            }
+        }
+    }
+)
